b49581c3100b489369ad8611fb75ff737b3db756
Nahuel Grijalba <nahug07@gmail.com>
2022-02-25 17:14:08 -0300

    Avance DataBase

---
+++ api/src/models/Temperament.js
@@ -0,0 +1,13 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("temperament", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+};
+ // no le paso el id porque se va a generar solo
